8e68be64cc7b50d98a5ee4f0bbbdd0babec32191
Julien Marrec <julien.marrec@gmail.com>
2021-08-20 17:54:28 +0200

    More testing including creating a structure on the fly

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include <gtest/gtest.h>
 
+#include "Filesystem.hpp"
 #include "FilesystemHelpers.hpp"
 #include "TestFixture.hpp"
 
-#include "Filesystem.hpp"
 #include <fmt/format.h>
-
 #include <vector>
 #include <iostream>
+#include <fstream>
 
 #include <boost/filesystem.hpp>
 
 namespace fs = openstudio::filesystem;
 
-bool copyDir(fs::path const& source, fs::path const& destination) {
+// @param ignoredPaths: a list of subfolders to ignore
+bool copyDir(fs::path const& source, fs::path const& destination, const std::vector<fs::path>& ignoredPaths = std::vector<fs::path>()) {
   try {
     // Check whether the function call is valid
     if (!fs::exists(source) || !fs::is_directory(source)) {
@@ -95,5 +96,86 @@ TEST_F(TestFixture, BoostCopy) {
     ASSERT_TRUE(fs::exists(d) && fs::is_regular_file(d));
     // Check that size is OK (will throw if either doesn't exist or regular file, hence the ASSERT above)
     EXPECT_EQ(fs::file_size(s), fs::file_size(d));
+  }
+}
+
+
+
+struct TestPath {
+  fs::path path;
+  bool allowed;
+};
+
+std::vector<TestPath> generateTestMeasurePaths() {
+
+  std::vector<TestPath> testPaths;
+
+  std::vector<std::string> approvedRootFiles {
+      "measure.rb", "README.md","README.md.erb",
+      "LICENSE.md", "measure.xml", ".gitkeep",
+  };
+
+  std::vector<std::string> approved_folders {
+    "docs", "resources", "tests"
+  };
+
+  std::vector<std::string> ignoredPaths {
+    "root_file.txt", "tests/output/output.txt", "subfolder/subfolder.txt",
+    ".git/index", ".hidden_folder/file.txt"
+  };
+
+  testPaths.reserve(approvedRootFiles.size() + 2*approved_folders.size() + ignoredPaths.size());
+  for (const auto& s: approvedRootFiles) {
+    testPaths.emplace_back(fs::path{s}, true);
+  }
+
+  for (const auto& s: approved_folders) {
+    fs::path file(s);
+    file.replace_extension(".rb");
+    testPaths.emplace_back(fs::path{s} / file, true);
+    testPaths.emplace_back(fs::path(s) / "subfolder" / "subfolder_file.txt", true);
+  }
+
+  for (const auto& s: ignoredPaths) {
+    testPaths.emplace_back(fs::path{s}, false);
+  }
+
+  return testPaths;
+}
+
+void createTestMeasureDirectory(const std::vector<TestPath>& testPaths) {
+
+  openstudio::path srcDir("../test/my_measure/");
+  auto absolutePath = fs::weakly_canonical(srcDir);
+  fmt::print("srcDir={}\n", srcDir);
+  fmt::print("absolutePath={}\n", absolutePath);
+  fs::remove_all(srcDir);
+  fs::create_directories(absolutePath);
+
+  for (const auto& testPath: testPaths) {
+    const fs::path& p = fs::weakly_canonical(absolutePath / testPath.path);
+    fmt::print("{}\n", p);
+    fs::create_directories(p.parent_path());
+    std::ofstream(p.string()) << p.string();
+  }
+}
+
+
+TEST_F(TestFixture, TestFull) {
+
+  std::vector<TestPath> testPaths = generateTestMeasurePaths();
+  EXPECT_GT(testPaths.size(), 5);
+  createTestMeasureDirectory(testPaths);
+
+}
+
+TEST_F(TestFixture, TestExclusion) {
+
+  auto abspath = fs::canonical(".");
+  openstudio::path parentPath = abspath;
+
+  while (!parentPath.empty()) {
+    fmt::print("{}\n", parentPath);
+    parentPath = parentPath.parent_path();
   }
 }
